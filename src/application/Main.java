54b057f7029a16d4c0978b18c6fee8de3d6c5958
Animator282 <55846018+Animator282@users.noreply.github.com>
2022-11-18 23:24:27 +0100

    BASE 0.3.1

--- src/application/Main.java
+++ src/application/Main.java
@@ -45,6 +45,7 @@ public class Main extends Application {
     
     
     
+    
     public static void main(String[] args) {
         launch(args);
     }
--- src/application/newListController.java
+++ src/application/newListController.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.scene.input.*;
 
 public class newListController {
+	
+	//Initializing the Main method
+	Main main = new Main();
 	
 	//Initialization of nodes
 	@FXML private VBox vboxL;
@@ -20,14 +26,23 @@ public class newListController {
 	@FXML public TextField rightField;
 	
 	//Arrays for storing Strings
-	public String[] leftWords = new String[1];
-	public String[] rightWords = new String[1];
+	public ArrayList<String> leftWords = new ArrayList<String>();
+	public ArrayList<String> rightWords = new ArrayList<String>();
 	
 	int elementArrays = 0;
+	
+	
+	public void enterKeyPressed(KeyEvent e) {
+		if(e.getCode() == KeyCode.ENTER)
+	    {
+	        addNewWord(null);
+	    }
+	}
 	
 	public void addNewWord(ActionEvent event) {
 		String leftWord = null;
 		String rightWord = null;
+		//Get text from text fields
 		try {
 			leftWord = leftField.getText();
 			rightWord = rightField.getText();
@@ -35,6 +50,22 @@ public class newListController {
 			System.out.println("Mfer failed");
 		}
 		System.out.println(leftWord + " " + rightWord);
+		
+		//Add text to specific array
+		leftWords.add(leftWord);
+		rightWords.add(rightWord);
+		
+		
+		Label addLabel1 = new Label((elementArrays + 1) + ": " + leftWord);
+		Label addLabel2 = new Label((elementArrays + 1) + ": " + rightWord);
+		
+		//Add label to GUI
+		vboxL.getChildren().add(addLabel1);
+		vboxR.getChildren().add(addLabel2);		
+		
+		//Count goes up by 1ß
+		elementArrays++;
+		
 	}
 	public void deleteWordPressed(ActionEvent event) {}
 	public void saveListPressed(ActionEvent event) {}
